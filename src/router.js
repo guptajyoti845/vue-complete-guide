944d0408967e10bb8d637dd76d0724e981dd81a0
Jyoti Gupta <jyotiguptaofficial@gmail.com>
2021-08-24 18:49:33 +0530

    initial vue setup

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { NotFound } from './pages/NotFound.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegister from './pages/coaches/CoachRegister.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }],
+    },
+    { path: '/register', component: CoachRegister },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
